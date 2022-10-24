f67b845ce4e0954a41303b901c73f2778ffbd2b2
zidanegimiga <zidanegimiga@gmail.com>
2022-10-24 20:20:14 +0300

    feat: add button variant

--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -2,19 +2,37 @@ import React, { MouseEvent } from "react";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
-  text: string;
-  active: boolean;
-  action?: MouseEvent
+  text?: string;
+  active?: boolean;
+  variant: string;
+  action?: MouseEvent;
 }
 
-const Button = ({ text, active, action}: ButtonProps) => {
-  return (
-    <div 
-      className={active ? styles['active'] + ' ' + styles['buttonWrapper'] : styles['buttonWrapper']}
-      onClick={() => action}>
-      <em>{text}</em>
-    </div>
-  );
+const Button = ({ text, active, action, variant }: ButtonProps) => {
+  if (variant == "normal") {
+    return (
+      <>
+        <div
+          className={
+            active
+              ? styles["active"] + " " + styles["buttonWrapper"]
+              : styles["buttonWrapper"]
+          }
+          onClick={() => action}
+        >
+          {text}
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className={styles.search} onClick={() => action}>
+          {text}
+        </div>
+      </>
+    );
+  }
 };
 
 export default Button;
