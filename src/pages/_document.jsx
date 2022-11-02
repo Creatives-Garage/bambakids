f82e6d94611b3064a1d80f582f664783f8c5ab22
zidanegimiga <zidanegimiga@gmail.com>
2022-11-02 14:39:41 +0300

    style: add global stylesheet to the root app - create custom document page to add fonts - import and mount ready components to the home page - remove overflow along x-axis in the home page

---
+++ src/pages/_document.jsx
@@ -0,0 +1,37 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta property="og:type" content="website" key="og-type" />
+          <meta
+            property="og:url"
+            content=""
+            key="og-url"
+          />
+          <meta
+            property="og:title"
+            content="Bamba Kids"
+            key="og-title"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700;800;900&family=Nunito:wght@400;500;600;700;800;900;1000&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import BambaKidsToggler from '../features/Home/BambaKidsToggler'
+import Hero from '../features/Home/Hero'
+import Button from '../shared/Button'
+import Nav from '../shared/Nav'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      Home Page
+      <Nav/>
+      <Hero/>
+      <BambaKidsToggler/>
     </div>
   )
 }
