7b1387b7b4f6ffa9a8a108375756071260009396
zidanegimiga <zidanegimiga@gmail.com>
2022-11-13 18:23:43 +0300

    feat: create Sheng' Card

---
+++ src/features/Sheng/ShengCard/ShengCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from "./ShengCard.module.scss";
+import Image from 'next/image';
+
+interface ShengCardProps {
+    sheng: string,
+    translation: string,
+    image: StaticImageData;
+}
+
+const ShengCard = ({sheng, translation, image}: ShengCardProps) => {
+  return (
+    <div className={styles.cardContainer}>
+      <h2>{sheng}</h2>
+      <Image src={image} alt={translation} width={242} height={272}/>
+      <h3>{translation}</h3>
+    </div>
+  )
+}
+
+export default ShengCard
