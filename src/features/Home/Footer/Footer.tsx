47d3cc1cf9941dde0cd41706dc48d5c0f3ca8c24
zidanegimiga <zidanegimiga@gmail.com>
2022-11-13 22:45:15 +0300

    style: change structure & add privacy policy link

--- src/features/Home/Footer/Footer.tsx
+++ src/features/Home/Footer/Footer.tsx
@@ -1,29 +1,37 @@
-import Image from 'next/image';
-import React from 'react';
-import styles from './Footer.module.scss';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import styles from "./Footer.module.scss";
 
 function Footer() {
   return (
     <div className={styles.footerWrapper}>
-        <div className={styles.footerLeft}>
-            <div className={styles.footerTextGroup}>
-              <p>Bamba Kids @2022</p>
-              <p>A Creatives Garage Production</p>
-            </div>
-            <Image src="/cgLogo.png" height={72} width={240} alt="Creatives Garage Logo" />
-        </div>
-        <div className={styles.footerRight}>
-          <div className={styles.footerRightText}>
-            <p>Connect with us</p>
-          </div>
+      <div className={styles.horizontal}></div>
+      <div className={styles.footerBottom}>
+        <div className={styles.footerBottomLeft}>
+          <Image
+            src="/cgLogo.png"
+            height={64}
+            width={200}
+            alt="Creatives Garage Logo"
+          />
           <div className={styles.socialmedia}>
             <div className={styles.facebook}></div>
             <div className={styles.instagram}></div>
             <div className={styles.twitter}></div>
           </div>
         </div>
+        <div className={styles.footerTextGroup}>
+          <p>© 2022 Bamba Kids</p>
+          <p style={{marginRight: "8px", marginLeft: "8px"}}> | </p> 
+          <p>A Creatives Garage Production</p>
+          <p style={{marginRight: "8px", marginLeft: "8px"}}> | </p>
+          <Link href="/privacy"><p className={styles.footerLink}>Privacy policy</p></Link>
+        </div>
+      </div>
+      <div className={styles.horizontal}></div>
     </div>
-  )
+  );
 }
 
 export default Footer;
