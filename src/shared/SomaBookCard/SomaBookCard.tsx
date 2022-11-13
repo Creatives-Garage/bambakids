910c4c579ae2f16da0d7f6fdede2a3f8cb727e2f
zidanegimiga <zidanegimiga@gmail.com>
2022-11-13 18:25:58 +0300

    feat: create soma book card

---
+++ src/shared/SomaBookCard/SomaBookCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./SomaBookCard.module.scss";
+
+function SomaBookCard() {
+  return (
+    <div className={styles.cardWrapper}></div>
+  )
+}
+
+export default SomaBookCard
