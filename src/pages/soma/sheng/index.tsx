56285e89f38f82795ff403ed915fac8e32154d7b
zidanegimiga <zidanegimiga@gmail.com>
2022-11-14 17:59:02 +0300

    create Tab components

---
+++ src/pages/soma/sheng/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import type { NextPage } from "next";
+import Nav from "../../../shared/Nav";
+import styles from "./sheng.module.scss";
+import ShengCarousel from "../../../features/Sheng/ShengCarousel";
+import { shengData } from "../../../shared/Data/shengData";
+
+const Sheng: NextPage = () => {
+  return (
+    <div>
+      <Nav />
+      <div className={styles.pageContent}>
+        <div className={styles.navigationHeader}>
+          <button className={styles.backbutton}></button>
+          <div className={styles.contentTitle}>
+            <h2>Learn Sheng&apos;</h2>
+          </div>
+        </div>
+        <div className={styles.carouselWrapper}>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sheng;
