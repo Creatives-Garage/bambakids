3aa1804fcde15029ec442fef5a26aca37729b9c7
zidanegimiga <zidanegimiga@gmail.com>
2022-11-14 13:54:20 +0300

    feat: create book cards

--- src/shared/SomaBookCard/SomaBookCard.tsx
+++ src/shared/SomaBookCard/SomaBookCard.tsx
@@ -1,10 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import styles from "./SomaBookCard.module.scss";
 
-function SomaBookCard() {
+interface SomaBookCardProps {
+  image: StaticImageData;
+  link: string;
+  bookName: string;
+}
+
+function SomaBookCard({image, link, bookName}: SomaBookCardProps) {
   return (
-    <div className={styles.cardWrapper}></div>
+    <div>
+      <Link href={link}>
+        <div className={styles.cardContainer}>
+          <div className={styles.cardWrapper}>
+            <Image src={image} width={354} height={500} alt={bookName} />
+          </div>
+          <div className={styles.cardButton}> Read </div>
+        </div>
+      </Link>
+    </div>
   )
 }
 
-export default SomaBookCard
+export default SomaBookCard;
