2c61890534a7eb08cf0804dbf03aa396c40f6697
zidanegimiga <zidanegimigatemp@gmail.com>
2022-11-24 11:14:47 +0300

    feat: implement video player

---
+++ src/features/Tazama/YoutubeEmbedder/YoutubeEmbedder.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import style from './YoutubeEmbedder.module.scss'
+import PropTypes from "prop-types";
+
+const YoutubeEmbedder = ({ embedId }: any) => (
+  <div className={style.videoResponsive}>
+    <iframe
+      width="640"
+      height="385"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="controls; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+);
+
+YoutubeEmbedder.propTypes = {
+  embedId: PropTypes.string.isRequired
+};
+
+export default YoutubeEmbedder;
