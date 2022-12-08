92da3484f7204dab37982a1b93a1e7114410ada3
zidanegimiga <zidanegimiga@gmail.com>
2022-12-08 03:50:57 +0300

    feat: increase the number of characters present on the toggler - a better job needs to be done on how to present them

--- src/features/Home/BambaKidsToggler/BambaToggler.tsx
+++ src/features/Home/BambaKidsToggler/BambaToggler.tsx
@@ -4,16 +4,34 @@ import styles from "./BambaKidsToggler.module.scss";
 
 const charactersData = [
   {
-    name: "Nambi",
+    name: "Trufena",
     description:
-      "Nambi is always up to mischief, trying to climb the neighbor’s trees to get fruits, chasing the chicken around the compound and always being saved from trouble by his older sister Koki. He loves playing football and can’t get enough of his mother’s chapati’s. He especially loves traveling to see his grandfather, Mzee Risto and listening to his many epic tales.",
+      "Misadventures of Trufena is a book that tells the story of Trufena and her mum in the farm. Trufena likes her mom's chicken served with delicious chapatis. Trufena's need to have chicken for dinner makes her take the initiative to catch a chicken in the farm, running around in hot pursuit. Will she succeed in capturing the chicken?",
     headshot: "/characters/nambi-headshot.png",
     fullpic: "/characters/nambi-full.png",
   },
   {
-    name: "Koki",
+    name: "Jebet",
     description:
-      "Koki is a 10 year old African girl with an incredibly inquisitive mind. She is always asking her mother many questions, reading books and loves to play with her younger brother Nambi. Teaching him new things like how to count and spell his favorite words, although half the time she is busy saving Nambi from his antics.",
+      "Jebet the Runner is a book that tells the story of a girl named Jebet who grows up showcasing her talent for running. She liked running from a young age and would compete with her dolls to some imaginary finishing line. She would cheer herself and had an imaginary commentator voiceover running in her mind. She would also run from home to school and back, which was two kilometers away. One day, her mom recieves a phone call from from the National Olympic Committee inviting Jebet to compete in the forthcoming international race.",
+    headshot: "/characters/koki-headshot.png",
+    fullpic: "/characters/koki-fullpic.png",
+  },
+  {
+    name: "Tanu",
+    description: "I'm different, but what if I wasn't? I think life would be dull and stars would be null... Being Tanu is the story of a young girl named Tanu, growing up with people and animals that are different from her. That doesn't faze her, she instead explores the differences while exploring her own uniqueness.",
+    headshot: "/characters/koki-headshot.png",
+    fullpic: "/characters/koki-fullpic.png",
+  },
+  {
+    name: "Ayanna",
+    description: "Too often, little girls have dreams that are crushed by society telling them what they can and cannot do. Ayanna the Makanga is a heartwarming reminder to girls everywhere that their dreams are valid and they should listen to their innermost voice. Ayanna loved the road and public transport but was always told that jobs in transport were for tough boys. Our heroine does not pay these naysayers any heed. She grows up to do exactly what she loves and ends up changing the industry as a result.",
+    headshot: "/characters/koki-headshot.png",
+    fullpic: "/characters/koki-fullpic.png",
+  },
+  {
+    name: "Kwame",
+    description: "Kwame is a giraffe who stands tall and is completely sure of himself. He sees himself as a lion though, brave and magnificent. Although his friends laugh and his enemies look at him with scorn, he stays true to himself and he ends up helping the whole savannah. Beyond having a beautiful message on self love,  Kwame the Giraffe paints a picture of the savannah and other facets of Kenya that makes it a beautiful country.",
     headshot: "/characters/koki-headshot.png",
     fullpic: "/characters/koki-fullpic.png",
   },
@@ -21,11 +39,20 @@ const charactersData = [
 
 function BambaKidsToggler() {
   const [activeCharacter, setActiveCharacter] = useState(0);
-  function showNambi() {
+  function showTrufena() {
     setActiveCharacter(0);
   }
-  function showKoki() {
+  function showJebet() {
     setActiveCharacter(1);
+  }
+  function showTanu() {
+    setActiveCharacter(2);
+  }
+  function showAyanna() {
+    setActiveCharacter(3);
+  }
+  function showKwame() {
+    setActiveCharacter(4);
   }
 
   return (
@@ -33,7 +60,7 @@ function BambaKidsToggler() {
       <div className={styles.togglerContainer}>
         <div className={styles.togglesContainer}>
           <div
-            onClick={() => showNambi()}
+            onClick={() => showTrufena()}
             className={
               activeCharacter == 0
                 ? styles["togglerButton"] + " " + styles["active"]
@@ -48,7 +75,7 @@ function BambaKidsToggler() {
             />
           </div>
           <div
-            onClick={() => showKoki()}
+            onClick={() => showJebet()}
             className={
               activeCharacter == 1
                 ? styles["togglerButton"] + " " + styles["active"]
@@ -57,6 +84,51 @@ function BambaKidsToggler() {
           >
             <Image
               src={charactersData[1].headshot}
+              alt="Koki"
+              width={210}
+              height={210}
+            />
+          </div>
+          <div
+            onClick={() => showTanu()}
+            className={
+              activeCharacter == 2
+                ? styles["togglerButton"] + " " + styles["active"]
+                : styles["togglerButton"]
+            }
+          >
+            <Image
+              src={charactersData[2].headshot}
+              alt="Koki"
+              width={210}
+              height={210}
+            />
+          </div>
+          <div
+            onClick={() => showAyanna()}
+            className={
+              activeCharacter == 3
+                ? styles["togglerButton"] + " " + styles["active"]
+                : styles["togglerButton"]
+            }
+          >
+            <Image
+              src={charactersData[3].headshot}
+              alt="Koki"
+              width={210}
+              height={210}
+            />
+          </div>
+          <div
+            onClick={() => showKwame()}
+            className={
+              activeCharacter == 4
+                ? styles["togglerButton"] + " " + styles["active"]
+                : styles["togglerButton"]
+            }
+          >
+            <Image
+              src={charactersData[4].headshot}
               alt="Koki"
               width={210}
               height={210}
@@ -92,6 +164,48 @@ function BambaKidsToggler() {
             >
               <Image
                 src={charactersData[1].fullpic}
+                alt="Koki"
+                width={360}
+                height={391}
+              />
+            </div>
+            <div
+              className={
+                activeCharacter == 2
+                  ? styles["photo"] + "" + styles["activePhoto"]
+                  : styles["photo"]
+              }
+            >
+              <Image
+                src={charactersData[2].fullpic}
+                alt="Koki"
+                width={360}
+                height={391}
+              />
+            </div>
+            <div
+              className={
+                activeCharacter == 3
+                  ? styles["photo"] + "" + styles["activePhoto"]
+                  : styles["photo"]
+              }
+            >
+              <Image
+                src={charactersData[3].fullpic}
+                alt="Koki"
+                width={360}
+                height={391}
+              />
+            </div>
+            <div
+              className={
+                activeCharacter == 4
+                  ? styles["photo"] + "" + styles["activePhoto"]
+                  : styles["photo"]
+              }
+            >
+              <Image
+                src={charactersData[4].fullpic}
                 alt="Koki"
                 width={360}
                 height={391}
