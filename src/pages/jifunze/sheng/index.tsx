c1d394c1d2d11df1d9bff3998e060f72b3eb1880
zidanegimiga <zidanegimiga@gmail.com>
2022-12-08 04:02:01 +0300

    feat: initialise jifunze page

---
+++ src/pages/jifunze/sheng/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useRouter } from "next/router";
+import type { NextPage } from "next";
+import Nav from "../../../shared/Nav";
+import styles from "./sheng.module.scss";
+import ShengCarousel from "../../../features/Sheng/ShengCarousel";
+import { shengData } from "../../../shared/Data/shengData";
+
+const Sheng: NextPage = () => {
+  const router = useRouter()
+  return (
+    <div>
+      <Nav />
+      <div className={styles.pageContent}>
+        <div className={styles.navigationHeader}>
+          <button className={styles.backbutton} onClick={()=> router.push("/soma")}> Back </button>
+          <div className={styles.contentTitle}>
+            <h2>Learn Sheng&apos;</h2>
+          </div>
+        </div>
+        <div className={styles.carouselWrapper}>
+          <ShengCarousel data={shengData} />          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sheng;
