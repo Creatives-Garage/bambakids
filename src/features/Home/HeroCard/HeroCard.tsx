3a4ca857cfedd0d2bd098f9be260739c14898986
zidanegimiga <zidanegimiga@gmail.com>
2022-12-09 11:53:05 +0300

    feat: add animations to hero cards upon openning

--- src/features/Home/HeroCard/HeroCard.tsx
+++ src/features/Home/HeroCard/HeroCard.tsx
@@ -1,7 +1,25 @@
 import React, {PropsWithChildren, useState, ReactNode} from 'react';
+import { motion } from "framer-motion"
 import { useRouter, Router } from 'next/router';
 import styles from './HeroCard.module.scss';
 
+const titleVariants: any = {
+  in: {
+    y: 10,
+    opacity: 0.9,
+    transition: {
+      duration: 0.9
+    }
+  },
+  center: {
+    y: 0,
+    opacity: 1,
+    transformOrigin: 'top',
+    transition: {
+      duration: 0.9
+    }
+  },
+};
 interface HeroCardProps{
   text: string;
   route: string;
@@ -12,13 +30,16 @@ function HeroCard({text, route, children}: HeroCardProps) {
   const router = useRouter();
 
   return (
-    <div
+    <motion.div
+      variants={titleVariants}
+      initial="in"
+      animate={["center"]}
       onClick={()=>router.push(route)} 
       className={styles.heroCardWrapper}
     >
       {children}
       <h2>{text}</h2>       
-    </div>
+    </motion.div>
   )
 }
 
