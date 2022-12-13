5abe152300202c79d741dae7ed9d269f749b7abf
zidanegimiga <zidanegimiga@gmail.com>
2022-12-13 16:34:19 +0300

    feat: create dyk page and add data

--- src/pages/jifunze/did-you-know/index.tsx
+++ src/pages/jifunze/did-you-know/index.tsx
@@ -2,17 +2,70 @@ import React from "react";
 import { useRouter } from "next/router";
 import type { NextPage } from "next";
 import Nav from "../../../shared/Nav";
+import { motion } from "framer-motion";
 import styles from "./did-you-know.module.scss";
+import Carousel from "../../../shared/Carousel/Carousel";
+import { didYouKnowData } from "../../../shared/Data/didYouKnow";
+import path from 'path';
+import fs from 'fs/promises';
 
-const Sheng: NextPage = () => {
-  const router = useRouter()
+const imageVariants: any = {
+  in: {
+    x: -100,
+    transition: {
+      duration: 0.9
+    }
+  },
+  center: {
+    x: 0,
+    transformOrigin: 'top',
+    transition: {
+      duration: 0.9
+    }
+  },
+};
+
+const DidYouKnow: NextPage = ({videos}: any) => {
+  const payload = videos?.videos;
+  const router = useRouter();
+
   return (
-    <div>
-      <Nav />
+    <div className={styles.page}>
+      <Nav payload={payload}/>
       <div className={styles.pageContent}>
+        <div className={styles.navigationHeader}>
+          <button className={styles.backbutton} onClick={()=> router.push("/jifunze/")}> Back </button>
+          <div className={styles.contentTitle}>
+            <motion.h2
+              variants={imageVariants}
+              initial="in"
+              animate={["center"]}
+            >Did You Know...?</motion.h2>
+          </div>
+        </div>
+        <div className={styles.carouselWrapper}>
+          <Carousel data={didYouKnowData} />          
+        </div>
       </div>
     </div>
   );
 };
 
-export default Sheng;
+export default DidYouKnow;
+
+async function getData() {
+  const filePath = path.join(process.cwd(), 'src', 'shared', 'Data', 'tazamaData.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData.toString());
+
+  return data;
+}
+
+export async function getServerSideProps(context: any) {
+  const payload = await getData();
+  return {
+    props: {
+      videos: payload,
+    },
+  };
+}
---
+++ src/shared/Data/didYouKnow.ts
@@ -0,0 +1,59 @@
+export const didYouKnowData = [
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/abacus.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/algeria.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/christians.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/fishing.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/fossils.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/gold.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/hippo.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/kenya.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/kilimanjaro.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/maasai.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/nigeria.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/nile.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/quarry.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/sahara.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/seychelles.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/snow.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/vic-falls.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/university.png"
+    },
+    {
+        image: "https://www.bambaapp.creativesgarage.org/public/assets/images/posts/tribe.png"
+    },
+]
