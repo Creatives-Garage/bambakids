7be2c4ca05e2abe7e61817f82e6069bcfda183c6
zidanegimiga <zidanegimiga@gmail.com>
2023-02-11 13:47:00 +0300

    config: optimise site for seo

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -14,16 +14,23 @@ class MyDocument extends Document {
             <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
             <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" /> 
           */}
+          <meta name="robots" content="all" />
+          <meta name="googlebot" content="index,follow" />
           <meta property="og:type" content="website" key="og-type" />
           <meta
             property="og:url"
-            content=""
+            content="bambakids.org"
             key="og-url"
           />
           <meta
             property="og:title"
             content="Bamba Kids"
             key="og-title"
+          />
+          <meta
+            property="og:description"
+            content="With the Bamba kids app , the young ones can read digital books, play educational games, be enlightened with general knowledge facts"
+            key="og-description"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
